7666d9f6384e511c895b30096eada20649b739fb
Richard Wibowo <richardmulyawan@hotmail.com>
2024-01-14 15:25:09 +1100

    new config

--- next.config.js
+++ next.config.js
@@ -3,14 +3,13 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com",'s3.us-west-2.amazonaws.com'],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
-
-module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   output: "standalone",
 };
+
+module.exports = nextConfig;
